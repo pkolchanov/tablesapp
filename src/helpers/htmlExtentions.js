83dfb962aeeebddca65d41d82822019a5b15e7b1
Pavel K <p.kolchanov@knopka.com>
2021-01-13 19:38:18 +0300

    donw key selection fix

--- src/helpers/htmlExtentions.js
+++ src/helpers/htmlExtentions.js
@@ -12,7 +12,7 @@ function getSelectionHeight(textarea) {
     var new_node = document.createElement("div");
     textarea.parentNode.insertBefore(new_node, textarea.nextSibling);
     new_node.style = `opacity:0;position:fixed;top:0;right;width:${textarea.offsetWidth}px;min-height:${lineHeight}px;`;
-    new_node.innerHTML = String(textarea.value.substring(0, textarea.selectionStart));
+    new_node.innerHTML = String(textarea.value.substring(0, textarea.selectionEnd));
     const offsetHeight = new_node.offsetHeight;
     new_node.remove();
     return offsetHeight;
