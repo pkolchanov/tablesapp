8cf91e2d484d8c2d45f0f5c86c4a4c7f6436b1bf
Pavel K <p.kolchanov@knopka.com>
2021-01-24 20:06:57 +0300

    Split webapp index to login and app pages

--- src_webapp/index.js
+++ src_webapp/index.js
@@ -8,42 +8,28 @@ import "firebase/database";
 import './styles/app.css';
 import {firebaseConfig} from "../src/helpers/firebaseConfig";
 
-if (window.location.pathname.startsWith('/finishSignUp')) {
-    //todo split to diffrenent pages
+firebase.initializeApp(firebaseConfig);
+
+if (location.hostname === "localhost") {
+    firebase.database().useEmulator("localhost", 9000);
+}
+
+const refSheet = action((snapshot) => {
+    const data = snapshot.val();
+    sheetStore.data = data.sheetData;
+    sheetStore.columnWidths = data.columnWidths;
+    sheetStore.activeCoords = data.activeCoords;
+})
+const tableId = window.location.pathname;
+const table_ref = firebase.database().ref('tables' + tableId);
+
+table_ref.on('value', refSheet);
+table_ref.once('value').then((snapshot) => {
+    refSheet(snapshot);
     render(
-        <div>
-            Redirecting you to tablesapp...
+        <div className={"app"}>
+            <Sheet isReadOnly={true}/>
         </div>,
         document.getElementById("root")
     );
-    const link = document.createElement('a');
-    link.href = `tablesapp://response=${window.location.href}`;
-    document.body.appendChild(link);
-    link.click();
-} else {
-    firebase.initializeApp(firebaseConfig);
-
-    if (location.hostname === "localhost") {
-        firebase.database().useEmulator("localhost", 9000);
-    }
-
-    const refSheet = action((snapshot) => {
-        const data = snapshot.val();
-        sheetStore.data = data.sheetData;
-        sheetStore.columnWidths = data.columnWidths;
-        sheetStore.activeCoords = data.activeCoords;
-    })
-    const tableId = window.location.pathname;
-    const table_ref = firebase.database().ref('tables' + tableId);
-
-    table_ref.on('value', refSheet);
-    table_ref.once('value').then((snapshot) => {
-        refSheet(snapshot);
-        render(
-            <div className={"app"}>
-                <Sheet isReadOnly={true}/>
-            </div>,
-            document.getElementById("root")
-        );
-    });
-}
+});
